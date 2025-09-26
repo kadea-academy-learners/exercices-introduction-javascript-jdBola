b9fb02b4efec7d11f9a4dc93f6375b722d315ce9
Jean Dino <boladino56@gmail.com>
2025-09-26 19:33:56 +0200

    exercice 7 ok

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,9 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 12;
+let agePere = ageEnfant * 2;
+let ageMere = agePere - 5 ;
+let ageGrandPere = (ageMere * 2) + ageEnfant/2;
+let ageOncle = agePere + 10;
+
+console.log("L'enfant a "+ ageEnfant +",le père a "+ agePere+",la mère a " + ageMere + "et l'oncle a "+ ageOncle);
