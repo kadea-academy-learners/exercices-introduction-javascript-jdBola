dd0ab2a85d5fa15432a08f20b0dbe62796026917
Jean Dino <boladino56@gmail.com>
2025-09-26 19:20:23 +0200

    exercice 6 OK

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,9 @@
 // Exercice 6
 // Écrivez votre code ici
+let age = 10;
+let nom = 'BOLA';
+let estConnecte = true;
+let valeurNulle = null;
+let inconue ;
+
+console.log(age+' '+ nom +' '+ estConnecte +' '+ valeurNulle +' '+ inconue);
