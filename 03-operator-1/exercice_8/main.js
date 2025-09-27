509cfbf7f331289b9fe412be34bcf02bb2e4e780
Jean Dino <boladino56@gmail.com>
2025-09-27 20:13:51 +0200

    exercice 8 ok

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,14 @@
 // Exercice 8
 // Écrivez votre code ici
+let salaireMensuel = 500;
+let loyer = (30 / 100) * salaireMensuel;
+let nourriture = (20 / 100) * salaireMensuel;
+let transport = (10 / 100) * salaireMensuel;
+let autresDepenses = 25;
+autresDepenses = autresDepenses + 50;
+let totaldepenses = loyer + nourriture + transport + autresDepenses ;
+let reste = salaireMensuel - totaldepenses;
+
+console.log("Le salaire est de "+ salaireMensuel+ "Le loyer est "+ loyer + "La nourriture est de "+ nourriture+"Le transport"+transport+"Les autres dédepenses sont à " +autresDepenses+" le total dépenses est de "+totaldepenses+
+    "le reste est "+reste
+);
